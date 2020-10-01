12da28d52b7a28ea346b7fba299c69a603f2f490
TobiasHasenkamp <tobias.hasenkamp@googlemail.com>
2020-10-01 11:32:58 +0200

    Simplified the getPeopleCapacityStatus-method.

--- src/main/java/de/neuefische/jan/j1exercises2409/Alarm.java
+++ src/main/java/de/neuefische/jan/j1exercises2409/Alarm.java
@@ -3,15 +3,11 @@ package de.neuefische.jan.j1exercises2409;
 public class Alarm {
 
     public static String getPeopleCapacityStatus(int numberOfPeople, AlarmLevel threatLevel) {
-        int limit = getMaximumNumberOfPeople(threatLevel);
+        int limit = threatLevel.maxCapacity;
         if (numberOfPeople <= limit) {
             return "Maximum number of people not exceeded";
         }
         return "Too many people";
     }
 
-    public static int getMaximumNumberOfPeople(AlarmLevel threatLevel){
-        return threatLevel.maxCapacity;
-
-    }
 }
